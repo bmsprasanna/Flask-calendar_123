5a59423534ac7cdc8ad8a03230de3bb65bd21745
bmsprasanna <prasannas2217@gmail.com>
2024-07-10 13:17:12 +0530

    commit changes

---
+++ static/js/main.js
@@ -0,0 +1,8 @@
+
+const prname = document.getElementById('project_name');
+const usernme = document.getElementById('user_name');
+datess = "{{ date_selected }}"
+console.log(datess)
+// usernme.addEventListener('input', function () {
+// 	textBoxToUpdate.value = this.value;
+// });
